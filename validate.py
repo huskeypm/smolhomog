f21d239b16487daacec3dd1bdb3c4f9e3e948835
huskeypm <huskeypm@gmail.com>
2013-08-01 15:35:46 -0700

    minor

--- validate.py
+++ validate.py
@@ -4,7 +4,6 @@
 
 
 
-import homog 
 import numpy as np
 from dolfin import * 
 
@@ -25,39 +24,81 @@ def FD2d(x,y,amplitude=1., marg=2., midpoint=5., slope=10.):
 
 
 
+  
+def doit():
+  import homog 
+  
+  ## Grid setup 
+  mesh = UnitSquare(50,50)
+  mesh.coordinates()[:] = mesh.coordinates()[:] * 10.
+  V = FunctionSpace(mesh,"CG",1) 
+  
+  ## subdomains (do nothing)
+  subdomains = MeshFunction("uint",mesh,1)
+  
+  ## PMF setup 
+  print "WARNING: need to use dof map here"
+  pmf  = Function(V)  
+  amplitude = 2. # [kcal/mol]
+  marg=3.;
+  midpoint=5.;
+  slope=10.
+  xs=mesh.coordinates()[:,0];ys=mesh.coordinates()[:,1]
+  
+  vfunc = np.vectorize(FD2d,otypes=[np.float])
+  pmfVals = vfunc(xs,ys, -1*amplitude,marg,midpoint,slope)
+  pmf.vector()[:] = pmfVals
+  
+  ## write files, but should be able to do this without writing files 
+  molPrefix = "temp"
+  meshFileInner = molPrefix+"_mesh.xml.gz"
+  subdomainFileInner = molPrefix+"_subdomains.xml.gz"
+  potentialFileInner = molPrefix+"_values.xml.gz"
+  File(meshFileInner) << mesh
+  File(subdomainFileInner) << subdomains
+  File(potentialFileInner) << pmf
+  
+  results = homog.SolveHomogSystem(
+    molPrefix=molPrefix,smolMode=True,molGamer=False)
 
 
-## Grid setup 
-mesh = UnitSquare(50,50)
-mesh.coordinates()[:] = mesh.coordinates()[:] * 10.
-V = FunctionSpace(mesh,"CG",1) 
+#!/usr/bin/env python
+import sys
+#
+# Revisions
+#       10.08.10 inception
+#
 
-## subdomains (do nothing)
-subdomains = MeshFunction("uint",mesh,1)
+if __name__ == "__main__":
+  import sys
+  scriptName= sys.argv[0]
+  msg="""
+Purpose: 
+ 
+Usage:
+"""
+  msg+="  %s -validation" % (scriptName)
+  msg+="""
+  
+ 
+Notes:
 
-## PMF setup 
-print "WARNING: need to use dof map here"
-pmf  = Function(V)  
-amplitude = 2. # [kcal/mol]
-marg=3.;
-midpoint=5.;
-slope=10.
-xs=mesh.coordinates()[:,0];ys=mesh.coordinates()[:,1]
+"""
+  remap = "none"
 
-vfunc = np.vectorize(FD2d,otypes=[np.float])
-pmfVals = vfunc(xs,ys, -1*amplitude,marg,midpoint,slope)
-pmf.vector()[:] = pmfVals
+  if len(sys.argv) < 2:
+      raise RuntimeError(msg)
 
-## write files, but should be able to do this without writing files 
-molPrefix = "temp"
-meshFileInner = molPrefix+"_mesh.xml.gz"
-subdomainFileInner = molPrefix+"_subdomains.xml.gz"
-potentialFileInner = molPrefix+"_values.xml.gz"
-File(meshFileInner) << mesh
-File(subdomainFileInner) << subdomains
-File(potentialFileInner) << pmf
+  fileIn= sys.argv[1]
+  if(len(sys.argv)==3):
+    print "arg"
 
-results = homog.SolveHomogSystem(
-  molPrefix=molPrefix,smolMode=True,molGamer=False)
+  for i,arg in enumerate(sys.argv):
+    if(arg=="-validation"):
+      doit()
+
+
+
+
 
 
