fixed significant bugs in handling potential sign

example/cylinder/cylinder3D.py
@@ -1,6 +1,6 @@
 import sys 
 import matplotlib.pylab as plt
-sys.path.append("/net/home/huskeypm/Sources/homogenization/")
+#sys.path.append("/net/home/huskeypm/Sources/homogenization/")
 import homoglight as hl
 import numpy as np
 
@@ -14,7 +14,7 @@ class Omega1(SubDomain):
       return True
 
 # Create mesh and define function space
-def testCase(filename="none",mode="cylinder",qSubstrate=1.,dim=3):
+def testCase(filename="none",mode="cylinder",qSubstrate=1.,dim=3,qenz=-3):
 
   # Create mesh and define function space
   mesh = Mesh(filename)
@@ -34,10 +34,10 @@ def testCase(filename="none",mode="cylinder",qSubstrate=1.,dim=3):
   #File("subdomain.xml.gz") << subdomains
 
   # potential # KIND OF DEBYEHUCKEL LIKE< BUT NOT CORRCT 
-  qenz = -1. 
-    
-  qenz = qSubstrate
-  qSubstrate = -1  
+  #qenz = -1. 
+  #  
+  #qenz = qSubstrate
+  #qSubstrate = -1  
     
   if(mode=="cylinder"):  
     expr = Expression("qenz*prefac*exp(-1*sqrt(x[0]*x[0]+x[1]*x[1])/w)",prefac=3,qenz=qenz,w=10)
@@ -48,8 +48,10 @@ def testCase(filename="none",mode="cylinder",qSubstrate=1.,dim=3):
     
   import numpy as np
   ar =   np.asarray(psi.vector()[:])
-  print "Fake min/max %f/%f " % (np.min(ar),np.max(ar))
-  psi.vector()[:] *= 0.593 # kT
+  print "Fake psi min/max %f/%f [mV]" % (np.min(ar),np.max(ar))
+  psi.vector()[:] *= (1/25.) *0.593 # 1/RT [mV] * kT 
+  ar =   np.asarray(psi.vector()[:])
+  print "Fake pmf min/max %f/%f [mV]" % (np.min(ar),np.max(ar))
   File("psi.pvd") << psi
 
     
@@ -93,6 +95,14 @@ def doit():
   plt.legend(loc=0)
 
   plt.gcf().savefig("sphere_vs_cylinder.png")  
+
+def test():
+  fileIn="/net/home/huskeypm/Sources/homogenization/example/volfracs/volFrac_0.10_mesh.xml.gz"
+  fileIn="/home/huskeypm/sources/homogenization/example/volfracs/volFrac_0.10_mesh.xml.gz"
+  q=1
+  #q=0
+  q=-1
+  resultsSphere = testCase(fileIn,mode="sphere",qSubstrate=q)
     
 
 #sphere
@@ -121,8 +131,7 @@ if __name__ == "__main__":
 
     if(arg=="-run"):
       doit()
+ 
+    if(arg=="-test"):
+     test()
 
-if(0):
-  fileIn="/net/home/huskeypm/Sources/homogenization/example/volfracs/volFrac_0.10_mesh.xml.gz"
-  q=-1
-  resultsSphere = testCase(fileIn,mode="sphere",qSubstrate=q)

example/validation/valid.py
@@ -27,7 +27,7 @@ parms = pb.parms
 parms.res =3       
 m_to_A = 1e10
  
-parms.z = -1.  # lig charge    [Chloride]
+parms.zLig = -1.  # lig charge    [Chloride]
     
 #F=96485.3365 # Faradays constant [C/mol]
 #R = 8.3143   # Gas const [J/mol K] 
@@ -233,6 +233,7 @@ def runCase(engine="testing"):
     print "boundaryPotential ", boundaryPotential
     # potential is mV 
     (V,potential)= pb.SolvePoissonBoltzmann(mesh,boundaryPotential=boundaryPotential)
+    ar = np.asarray(potential.vector()); print "min/max %f/%f" %(np.min(ar),np.max(ar)) 
     
     #(gy,interp) = interp2d(mesh,potential)
     #plt.figure()
@@ -247,16 +248,19 @@ def runCase(engine="testing"):
     
     # multiply potential [mV] by F/RT [1/mV] s.t. potential is unitless 
     unitlessPotential = Function(V)
-    unitlessPotential.vector()[:] = parms.Fz_o_RT*potential.vector()[:] 
+    #unitlessPotential.vector()[:] = parms.Fz_o_RT*potential.vector()[:] 
+    unitlessPotential.vector()[:] = parms.zLig*parms.F_o_RT*potential.vector()[:] 
+    ar = np.asarray(unitlessPotential.vector()); print "min/max %f/%f" %(np.min(ar),np.max(ar)) 
     
     parms.update()
     if(engine=="testing"):
       results = test.doit(mode="hack2",discontinuous=False,dim=2,fileIn=fileIn,potential=unitlessPotential)
     elif(engine=="homog.py"):
       scaledPotential = Function(V)  
-      scaledPotential.vector()[:] = parms.Fz_o_RT*potential.vector()[:]  
+      #scaledPotential.vector()[:] = parms.Fz_o_RT*potential.vector()[:]  
+      scaledPotential.vector()[:] = parms.F_o_RT*potential.vector()[:]  
       scaledPotential.vector()[:] = parms.kT * scaledPotential.vector()[:]  # (z=+,psi=+) --> V
-      results = hl.runHomog(fileXML=fileIn,psi=scaledPotential,q=1,smolMode=True)
+      results = hl.runHomog(fileXML=fileIn,psi=scaledPotential,q=parms.zLig,smolMode=True)
       results.Ds =results.d_eff 
     
     return results 
@@ -378,7 +382,7 @@ def validation(runSeveral=True):
     ## using 'runCase'
     results = runCase() 
     Drun1 = results.Ds[0]
-    print "runCase:sigma %f z %f D %f " %(sigma,parms.z,Drun1)
+    print "runCase:sigma %f z %f D %f " %(sigma,parms.zLig,Drun1)
 
     assert(np.abs(results.Ds[0] - Ds130813[i])<0.01), "Validation case FAILED! Do not commit!!"
     print "Success!"
@@ -389,7 +393,7 @@ def validation(runSeveral=True):
     # then multiply by -kT and assume that 'q=1' (since already included in Fz/RT term)
     results2 = runCase(engine="homog.py")
     Drun2 = results2.Ds[0]
-    print "homog: sigma %f z %f D %f " %(sigma,parms.z,Drun2)
+    print "homog: sigma %f z %f D %f " %(sigma,parms.zLig,Drun2)
 
     assert(np.abs(Drun1-Drun2)<0.001)
 
@@ -416,7 +420,7 @@ def test1():
     Ds[i] = results.Ds[0]
 
   plt.figure()
-  plt.plot(sigmas,Ds,'b',label="z=%3.1f"%parms.z)           
+  plt.plot(sigmas,Ds,'b',label="z=%3.1f"%parms.zLig)           
   plt.xlabel("$\sigma$ $[C/m^2]$")
   plt.ylabel("D")
   plt.legend(loc=0)
@@ -470,6 +474,13 @@ Notes:
   for i,arg in enumerate(sys.argv):
     if(arg=="-validation"): 
       validation()
+    if(arg=="-doAll"): 
+      #fig3()
+      #fig7()
+      #fig7Murad()
+      #fig8()
+      fig9()
+      quit()
 
     if(arg=="-fig3"):
       fig3()